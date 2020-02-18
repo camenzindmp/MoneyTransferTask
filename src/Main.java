7478fb92839d2d1f60c117a187a0cabf9556ca5e
Maksim <camenzindmp@gmail.com>
2020-02-18 20:36:32 +1000

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int originalBalance = 2_000_000_000;
+        int transferAmount = 500_000_000;
+        int currentBalance = originalBalance + transferAmount;
+        System.out.println(currentBalance);
+    }
+}
